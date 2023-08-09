66fccdeab6e6d6408afeae4d8f896ad8a4831b25
Moritz Bergemann <moritz.bergemann@gmail.com>
2023-08-09 23:13:01 +0800

    feat: start events page + carousels

---
+++ src/lib/EventCarousel.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import emblaCarouselSvelte from 'embla-carousel-svelte';
+	import Autoplay from 'embla-carousel-autoplay';
+
+	let options = { loop: true };
+	let plugins = [Autoplay({delay: 4000})];
+
+	onMount(() => {});
+</script>
+
+<div class="embla">
+	<div class="embla__viewport" use:emblaCarouselSvelte={{ options, plugins }}>
+		<div class="embla__container">
+			<div class="embla__slide">
+				<img
+					class="embla__slide__img"
+					src="/assets/events/board-games-night-2023/3.JPG"
+					alt="Your alt text"
+				/>
+			</div>
+			<div class="embla__slide">
+				<img
+					class="embla__slide__img"
+					src="/assets/events/board-games-night-2023/4.JPG"
+					alt="Your alt text"
+				/>
+			</div>
+			<div class="embla__slide">
+				<img
+					class="embla__slide__img"
+					src="/assets/events/board-games-night-2023/6.JPG"
+					alt="Your alt text"
+				/>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+	.embla {
+		--slide-spacing: 10rem;
+		--slide-size: 75%;
+		--slide-height: 24rem;
+		padding: 1.6rem;
+	}
+	.embla__viewport {
+		overflow: visible;
+	}
+	.embla__container {
+		backface-visibility: hidden;
+		display: flex;
+		touch-action: pan-y;
+		margin-left: calc(var(--slide-spacing) * -1);
+	}
+	.embla__slide {
+		flex: 0 0 var(--slide-size);
+		min-width: 0;
+		padding-left: var(--slide-spacing);
+		position: relative;
+	}
+	.embla__slide__img {
+		display: block;
+		height: var(--slide-height);
+		width: 100%;
+		object-fit: cover;
+	}
+</style>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,22 +1,29 @@
 <script lang="ts">
 	import NavBar from '$lib/NavBar.svelte';
+
+    import EventCarousel from '$lib/EventCarousel.svelte';
 </script>
 
 <body>
 	<NavBar dynamic={false}/>
 
     <div class="content">
-        <div class="divider"/>
         <h1>Events</h1>
+        
+        <h2>Board Games Night 2023</h2>
+        <EventCarousel/>
 
-        <div>
-            <p>Events go here</p>
-        </div>
+        <h2>Board Games Night 2024</h2>
+        <EventCarousel/>
 	</div>
 </body>
 
 <style>
     .divider {
         height: 2rem;
+    }
+
+    h2 {
+        margin: 0;
     }
 </style>
