022d5c6fe867cf6753c0fbc33c19f5959dc36ef6
Moritz Bergemann <moritz.bergemann@gmail.com>
2023-08-15 15:37:34 +0800

    feat: add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
-	import '../styles/app.css';
+	import Footer from '$lib/components/Footer.svelte';
+import '../styles/app.css';
 </script>
 
+<svelte:head>
+	<title>ComSSA</title>
+	<meta name="description" content="Computer Science Student's Association at Curtin University"/>
+	<meta name="keywords" content="Computing, Students, Club, Association, Curtin University"/>
+</svelte:head>
+
 <slot />
+
+<Footer/>
